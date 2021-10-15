cf05a78d4c349d1dd2324a41f294668ce8619a8e
qixia1998 <qixia17138552@163.com>
2021-10-15 18:03:45 +0800

    updated

--- ch01/hello_typescript.ts
+++ ch01/hello_typescript.ts
@@ -1,6 +1,23 @@
-// Hello TypeScript
-console.log(`hello TypeScript`);
+//
+// Hello typescript
+//
+
+var version = `es6`
+console.log(`hello ${version} TypeScript`);
+
+var myString: string = `this is a string`;
+
+// myString = 1; // generates TS2322
 
 //
-var version = `es6`;
-console.log(`hello ${version} TypeScript`);
+// Basic types
+//
+
+var myBoolean : boolean = true;
+var myNumber : number = 1234;
+var myStringArray : string[] = [`first`, `second`, `third`];
+
+// myBoolean = myNumber;
+// myStringArray = myNumber;
+// myNumber = myStringArray[0];
+
---
+++ ch01/test_javascript.js
@@ -0,0 +1,10 @@
+var test = "a string";
+console.log("test = " + test);
+
+test = 1;
+console.log("test = " + test);
+
+test = function (a, b) {
+    return a + b;
+}
+console.log("test = " + test);
