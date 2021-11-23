1d93f714aadf563ea2971a867920a6d00a14c9fa
qixia1998 <qixia17138552@163.com>
2021-11-23 11:18:33 +0800

    updated type_inference

--- ch04/type_inference.ts
+++ ch04/type_inference.ts
@@ -107,3 +107,100 @@ console.log(`keyAb = ${keyAb}`);
 
 let keyAbc = getTupleStringAbc<IAbc>([1, "test", true]);
 console.log(`keyAbc = ${keyAbc}`);
+
+// 
+// distributed conditional types
+//
+
+type dateNumberOrString<T> = 
+    T extends Date ? Date :
+    T extends number ? Date | number :
+    T extends string ? Date | number | string :
+    never;
+
+function compareValues
+    <T extends string | number | Date | boolean>
+(
+    input: T, compareTo: dateNumberOrString<T>
+) {
+    // do comparison
+}
+
+compareValues(new Date(), new Date());
+compareValues(1, new Date());
+compareValues(1, 2);
+compareValues("test", new Date());
+compareValues("test", 1);
+compareValues("test", "test");
+
+//
+// conditional type inference
+//
+
+type inferFromPropertyType<T> = 
+    T extends { id: infer U } ? U : never;
+
+function testInferFromPropertyType<T>(
+    arg: inferFromPropertyType<T>
+) { }
+
+testInferFromPropertyType<{ id: string }>("test");
+testInferFromPropertyType<{ id: number }>(1);
+
+//
+// type inference from function signatures
+//
+
+type inferredFromFnParam<T> = 
+    T extends (a: infer U) => void ? U : never;
+
+function testInferredFromFnParam<T>(
+    arg: inferredFromFnParam<T>
+) { }
+
+testInferredFromFnParam<(a: number) => void>(1);
+testInferredFromFnParam<(a: string) => void>("test");
+
+
+type inferredFromFnReturnType<T> = 
+    T extends (a: string) => infer U ? U : never;
+
+function testInferredFromReturnType<T>(
+    arg: inferredFromFnReturnType<T>
+) { }
+
+testInferredFromReturnType<(a: string) => number>(1);
+testInferredFromReturnType<(a: string) => boolean>(false);
+
+//
+// type inference from arrays
+//
+
+type inferredTypeFromArray<T> = T extends (infer U)[] ? U : never;
+
+function testInferredFromArray<T>(args: inferredTypeFromArray<T>) { }
+
+testInferredFromArray<string[]>("test");
+testInferredFromArray<number[]>(1);
+
+//
+// standard conditional types
+//
+
+// exclude
+type ExcludeStringAndNumber = Exclude<
+    string | number | boolean,
+    string | number>;
+
+let boolValue: ExcludeStringAndNumber = true;
+
+// extract
+type StringOrNumber = Extract<
+    string | boolean | never,
+    string | number>;
+
+let stringValue: StringOrNumber = "test";
+
+// nonnullable
+type NotNullOrUndef = NonNullable<number | undefined | null>;
+let numValue: NotNullOrUndef = 1;
