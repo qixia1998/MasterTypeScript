ce4f3f3d112442217330eb06f472e899f00a57ae
qixia1998 <qixia17138552@163.com>
2021-11-23 14:21:38 +0800

    updated

---
+++ ch05/callbacks.js
@@ -0,0 +1,21 @@
+//
+// callbacks
+//
+function delayedResponseWithCallback(callback) {
+    function executeAfterTimeout() {
+        console.log("5. executeAfterTimeout()");
+        callback();
+    }
+    console.log("2. calling setTimeout");
+    setTimeout(executeAfterTimeout, 1000);
+    console.log("3. after calling setTimeout");
+}
+function callDelayedAndWait() {
+    function afterWait() {
+        console.log("6. afterWait()");
+    }
+    console.log("1. calling delayedResponseWithCallback");
+    delayedResponseWithCallback(afterWait);
+    console.log("4. after calling delayedResponseWithCallback");
+}
+callDelayedAndWait();
